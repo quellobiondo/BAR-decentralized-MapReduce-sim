67491e4f74571d648139e74424e89602164e4cfd
quellobiondo <alberto.zirondelli@studio.unibo.it>
2018-09-01 06:24:21 +0200

    Clean of unnecessary files, improved data analysis scripts

--- analysis/CompletionTime.R
+++ analysis/CompletionTime.R
@@ -12,27 +12,126 @@ data <- read_csv(file_name, col_names = TRUE, trim_ws = TRUE, col_types =
                      MapDuration = col_double(),
                      ReduceDuration = col_double(),
                      TotalDuration = col_double(),
-                     Updated = col_logical(),
-                     Config = col_character()
+                     Config = col_character(),
+                     Seed = col_integer()
                    )
-) %>% mutate(CompleteTopology=paste(Topology, NumberOfNodes))
+) %>% mutate(CompleteTopology=paste(Topology, NumberOfNodes), Id=paste(Platform, Config))
 
 # data <- filter(data, Config=="sumallcopies"|Config=="sumlowblocktime")
-data <- filter(data, NumberOfNodes==10, Platform!="MARS_S")
-ggplot(data, aes(Byzantine, TotalDuration, color = Platform)) +
+# data <- filter(data, NumberOfNodes==10, Platform=="MARS_M" | Platform=="MARS_M_PESSIMISTIC" )
+data10 <- filter(data, Number)
+
+# Aggregate the information to get a list of values for each combination of Config-Platform-PercentageOfByz
+agg_data10 <- data %>% group_by(CompleteTopology, Platform, Config, Byzantine, Id) %>% 
+  summarize(TotalDurationMean=mean(TotalDuration), TotalDurationMax=max(TotalDuration), TotalDurationMin=min(TotalDuration))
+
+ggplot(agg_data, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = agg_data$TotalDurationMin, ymax=agg_data$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), ylim = c(0, 600), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_10_with_interval_complete.jpg")
+
+ggplot(agg_data, aes(Byzantine, TotalDurationMean, color = Platform)) +
   facet_wrap(~CompleteTopology) +
   geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
   geom_line(aes(linetype = Config), na.rm = TRUE, se = FALSE) +
-  coord_cartesian(xlim = c(0, 40), ylim = c(0, 400), expand = TRUE) + 
+  coord_cartesian(xlim = c(0, 40), expand = TRUE) + 
   labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
-ggsave("completion_time_10.jpg")
+ggsave("completion_time_10_no_interval_complete.jpg")
 
-data <- filter(data, Byzantine <= 30)
+just_one_blocktime <- filter(agg_data, Config=="sum"|Config=="sum_no_real_byz")
+ggplot(just_one_blocktime, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = just_one_blocktime$TotalDurationMin, ymax=just_one_blocktime$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), ylim = c(0, 600), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_10_with_interval_only_sum.jpg")
 
-ggplot(data, aes(Byzantine, TotalDuration, color = Platform)) +
+just_hadoop_sum <- filter(agg_data, (Config=="sum"|Config=="sum_no_real_byz")&(Platform=="HADOOP" | Platform=="HADOOP_BFT"))
+just_lowest_blocktime <- filter(agg_data, (Config=="sumzeroblocktime"|Config=="sumzeroblocktime_no_real_byz")&(Platform!="HADOOP" & Platform!="HADOOP_BFT"))
+hadoop_sum_with_blockchain_lowest_time <- rbind(just_hadoop_sum, just_lowest_blocktime)
+ggplot(hadoop_sum_with_blockchain_lowest_time, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = hadoop_sum_with_blockchain_lowest_time$TotalDurationMin, ymax=hadoop_sum_with_blockchain_lowest_time$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_10_with_interval_hadoop_with_sum_blockchain_with_lowest_blocktime.jpg")
+
+selection_agg_data <- filter(agg_data, Byzantine <= 30)
+ggplot(selection_agg_data, aes(Byzantine, TotalDurationMean, color = Platform)) +
   facet_wrap(~CompleteTopology) +
   geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
   geom_line(aes(linetype = Config), na.rm = TRUE, se = FALSE) +
   coord_cartesian(xlim = c(0, 30), ylim = c(0, 310), expand = TRUE) + 
   labs(title = "Completion time increase with % byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
-ggsave("completion_time_10_detail.jpg")
+ggsave("completion_time_10_detail_no_interval_complete.jpg")
+
+data100 <- filter(data, NumberOfNodes==100, TotalDuration > 0)
+
+# Aggregate the information to get a list of values for each combination of Config-Platform-PercentageOfByz
+agg_data100 <- data %>% group_by(CompleteTopology, Platform, Config, Byzantine, Id) %>% 
+  summarize(TotalDurationMean=mean(TotalDuration), TotalDurationMax=max(TotalDuration), TotalDurationMin=min(TotalDuration))
+
+ggplot(agg_data100, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = agg_data100$TotalDurationMin, ymax=agg_data100$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), ylim = c(0, 600), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_with_interval_complete.jpg")
+
+ggplot(agg_data100, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE, se = FALSE) +
+  coord_cartesian(xlim = c(0, 40), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_no_interval_complete.jpg")
+
+just_one_blocktime <- filter(agg_data100, Config=="sum"|Config=="sum_no_real_byz")
+ggplot(just_one_blocktime, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = just_one_blocktime$TotalDurationMin, ymax=just_one_blocktime$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), ylim = c(0, 600), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_with_interval_only_sum.jpg")
+
+just_hadoop_sum <- filter(agg_data100, (Config=="sum"|Config=="sum_no_real_byz")&(Platform=="HADOOP" | Platform=="HADOOP_BFT"))
+just_lowest_blocktime <- filter(agg_data100, (Config=="sumzeroblocktime"|Config=="sumzeroblocktime_no_real_byz")&(Platform!="HADOOP" & Platform!="HADOOP_BFT"))
+hadoop_sum_with_blockchain_lowest_time <- rbind(just_hadoop_sum, just_lowest_blocktime)
+ggplot(hadoop_sum_with_blockchain_lowest_time, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE) +
+  geom_ribbon(aes(ymin = hadoop_sum_with_blockchain_lowest_time$TotalDurationMin, ymax=hadoop_sum_with_blockchain_lowest_time$TotalDurationMax, group = Id), linetype=2, alpha=0.1) +
+  coord_cartesian(xlim = c(0, 40), expand = TRUE) + 
+  labs(title = "Completion time increases with % of byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_with_interval_hadoop_with_sum_blockchain_with_lowest_blocktime.jpg")
+
+selection_agg_data <- filter(agg_data100, Byzantine <= 30)
+ggplot(selection_agg_data, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE, se = FALSE) +
+  coord_cartesian(xlim = c(0, 30), ylim = c(0, 310), expand = TRUE) + 
+  labs(title = "Completion time increase with % byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_detail_30_no_interval_complete.jpg")
+
+selection_agg_data <- filter(agg_data100, Byzantine <= 10)
+ggplot(selection_agg_data, aes(Byzantine, TotalDurationMean, color = Platform)) +
+  facet_wrap(~CompleteTopology) +
+  geom_point(aes(shape = Config), size=3, na.rm = TRUE) +
+  geom_line(aes(linetype = Config), na.rm = TRUE, se = FALSE) +
+  coord_cartesian(xlim = c(0, 10), ylim = c(0, 310), expand = TRUE) + 
+  labs(title = "Completion time increase with % byzantine nodes and block creation time", x="% Byzantine nodes", y="Job Duration")
+ggsave("completion_time_100_detail_10_no_interval_complete.jpg")
--- analysis/ResourcesUsage.R
+++ analysis/ResourcesUsage.R
@@ -11,17 +11,30 @@ cpu_data <- read_csv(file_name, col_names = TRUE, trim_ws = TRUE, col_types =
                      Byzantine = col_integer(),
                      Power = col_double(),
                      Time = col_double(),
-                     Updated = col_logical(),
-                     Config = col_character()
+                     Config = col_character(),
+                     Seed = col_integer()
                    )
 ) %>%   mutate(PowerTime=Time*Power) %>%
-        mutate(CompleteTopology=paste(Topology, NumberOfNodes)) 
+        mutate(CompleteTopology=paste(Topology, NumberOfNodes), Id=paste(Platform, Config)) 
 
-cpu_data <- filter(cpu_data, NumberOfNodes == 10, Platform!="MARS_S")
+cpu_data_10 <- filter(cpu_data, NumberOfNodes == 10)
 
-# %>% filter(Config=="sumallcopies" | Config=="sumlowblocktime")
+by_configuration <- group_by(cpu_data_10, CompleteTopology, Platform, Config, Byzantine, Id)
+cpu_active <- filter(by_configuration, Power>0) %>% summarise(total_cpu_time = sum(Time, na.rm = TRUE), total_power = sum(PowerTime, na.rm = TRUE))
+cpu_idle <- filter(by_configuration, Power==0) %>% summarize(total_idle_time = sum(Time, na.rm = TRUE))
 
-by_configuration <- group_by(cpu_data, CompleteTopology, Platform, Config, Byzantine)
+cpu_active_aggregated <- cpu_active %>% group_by(CompleteTopology, Platform, Config, Byzantine, Id) %>% 
+  summarize(TotalCpuTimeMean=mean(total_cpu_time), TotalCpuTimeMax=max(total_cpu_time), TotalCpuTimeMin=min(total_cpu_time))
+
+
+ggplot(cpu_active, aes(Platform, total_cpu_time, fill = Byzantine, alpha  = Config)) +
+  facet_wrap(~CompleteTopology) +
+  geom_bar(stat = 'identity', position = 'dodge2') +
+  labs(x="Platforms", y="Total CPU Time",title = "CPU time increases with % of byzantine nodes")
+ggsave("resource_usage_full_10.jpg")
+
+cpu_data_10_sum <- filter(cpu_data_10, Config=="sum" | Config=="sum_no_real_byz")
+by_configuration <- group_by(cpu_data_10_sum, CompleteTopology, Platform, Config, Byzantine)
 cpu_active <- filter(by_configuration, Power>0) %>% summarise(total_cpu_time = sum(Time, na.rm = TRUE), total_power = sum(PowerTime, na.rm = TRUE))
 cpu_idle <- filter(by_configuration, Power==0) %>% summarize(total_idle_time = sum(Time, na.rm = TRUE))
 
@@ -29,7 +42,7 @@ ggplot(cpu_active, aes(Platform, total_cpu_time, fill = Byzantine, alpha  = Conf
   facet_wrap(~CompleteTopology) +
   geom_bar(stat = 'identity', position = 'dodge2') +
   labs(x="Platforms", y="Total CPU Time",title = "CPU time increases with % of byzantine nodes")
-ggsave("resource_usage.jpg")
+ggsave("resource_usage_full_10_only_sum.jpg")
 
 
 ggplot(cpu_idle, aes(Platform, total_idle_time, fill = Byzantine, alpha = Config)) +
@@ -39,7 +52,7 @@ ggplot(cpu_idle, aes(Platform, total_idle_time, fill = Byzantine, alpha = Config
 ggsave("resource_unused.jpg")
 
 # Other data analysis on Idle time of a specificPlatform
-specific_file_name="data/Cluster-10/variable.HADOOP_BFT-30-sum.csv"
+specific_file_name="data/Cluster-10/variable.HADOOP-0-sum-0.csv"
 # Variable, chimint-16.lille.grid5000.fr, power, 0.000000, 648.167829, 648.167829, 23530999808.000000
 specific_cpu_data <- read_csv(specific_file_name, col_names = FALSE, trim_ws = TRUE, col_types = 
                        cols(
@@ -57,11 +70,65 @@ ggplot(specific_cpu_data, aes(X6, color = X2)) +
   geom_histogram(binwidth = 1, na.rm = TRUE) +
   labs(title = "Distribution of waiting time durations during the experiment, with blocktime = 15s and 20% Byzantines", x="Duration Idle Time", color="Node")
 
-ggsave("HADOOP_BFTByz-full-replicas-idle-histogram.jpg")
+ggsave("HADOOP-idle-histogram.jpg")
 
 ggplot(specific_cpu_data, aes(X4, X6, color = X2)) +
   geom_jitter(size=1, na.rm = TRUE) +
   coord_cartesian(ylim = c(0, 110), expand = TRUE) + 
   labs(title = "Block time creation delays the start of the reduce phase and the job completion", x="Start Event Time", y="Idle Time Duration", color="Node")
  
-ggsave("HADOOP_BFTByz-full-replicas-idle-points.jpg")
+ggsave("HADOOP-idle-points.jpg")
+
+specific_cpu_data_MARS_0_file_name="data/Cluster-10/variable.MARS_M-0-sum-0.csv"
+# Variable, chimint-16.lille.grid5000.fr, power, 0.000000, 648.167829, 648.167829, 23530999808.000000
+specific_cpu_data_MARS_0 <- read_csv(specific_cpu_data_MARS_0_file_name, col_names = FALSE, trim_ws = TRUE, col_types = 
+                                cols(
+                                  X1 = col_character(),
+                                  X2 = col_character(),
+                                  X3 = col_character(),
+                                  X4 = col_double(),
+                                  X5 = col_double(),
+                                  X6 = col_double(),
+                                  X7 = col_double()
+                                )
+) %>% filter(X3=="power_used" & X7==0) 
+
+ggplot(specific_cpu_data_MARS_0, aes(X6, color = X2)) +
+  geom_histogram(binwidth = 1, na.rm = TRUE) +
+  labs(title = "Distribution of waiting time durations during the experiment, with blocktime = 15s and 0% Byzantines", x="Duration Idle Time", color="Node")
+
+ggsave("MARS-0byz-idle-histogram.jpg")
+
+ggplot(specific_cpu_data_MARS_0, aes(X4, X6, color = X2)) +
+  geom_jitter(size=1, na.rm = TRUE) +
+  coord_cartesian(ylim = c(0, 110), expand = TRUE) + 
+  labs(title = "Block time creation delays the start of the reduce phase and the job completion", x="Start Event Time", y="Idle Time Duration", color="Node")
+
+ggsave("MARS-0byz-idle-points.jpg")
+
+specific_cpu_data_MARS_30_file_name="data/Cluster-10/variable.MARS_M-30-sum-0.csv"
+# Variable, chimint-16.lille.grid5000.fr, power, 0.000000, 648.167829, 648.167829, 23530999808.000000
+specific_cpu_data_MARS_30 <- read_csv(specific_cpu_data_MARS_30_file_name, col_names = FALSE, trim_ws = TRUE, col_types = 
+                                       cols(
+                                         X1 = col_character(),
+                                         X2 = col_character(),
+                                         X3 = col_character(),
+                                         X4 = col_double(),
+                                         X5 = col_double(),
+                                         X6 = col_double(),
+                                         X7 = col_double()
+                                       )
+) %>% filter(X3=="power_used" & X7==0) 
+
+ggplot(specific_cpu_data_MARS_30, aes(X6, color = X2)) +
+  geom_histogram(binwidth = 1, na.rm = TRUE) +
+  labs(title = "Distribution of waiting time durations during the experiment, with blocktime = 15s and 30% Byzantines", x="Duration Idle Time", color="Node")
+
+ggsave("MARS-30byz-idle-histogram.jpg")
+
+ggplot(specific_cpu_data_MARS_30, aes(X4, X6, color = X2)) +
+  geom_jitter(size=1, na.rm = TRUE) +
+  coord_cartesian(ylim = c(0, 110), expand = TRUE) + 
+  labs(title = "Block time creation delays the start of the reduce phase and the job completion", x="Start Event Time", y="Idle Time Duration", color="Node")
+
+ggsave("MARS-30byz-idle-points.jpg")
